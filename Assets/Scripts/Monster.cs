4c49152cac3a35491f8d41129d969eaf5f1ba9a7
이채빈 <dlcoqls002@gmail.com>
2025-11-07 18:19:49 +0900

    monsterSpawner

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    public float destroyY = -0.5f;
+
+    void Start()
+    {
+        float randomX = Random.Range(-0.4f, 0.4f);
+        transform.position = new Vector3(randomX, 2, -7.5f);
+    }
+    private void Update()
+    {
+        transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+
+        if (transform.position.y < destroyY)
+
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MonsterSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    public float spawnInterval = 0.7f;
+
+    private float timer = 0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        //CBL: every 1.5 seconds
+        if (timer>=spawnInterval)
+        {
+            timer = 0f;
+            spawnMoster();
+            
+        }
+    }
+    void spawnMoster()
+    {
+        float randomX = Random.Range(-0.4f, 0.4f);
+        Vector2 spawnPos = new Vector2(randomX, 2f);
+        Instantiate(monsterPrefab, spawnPos, Quaternion.identity);
+
+    }
+}
