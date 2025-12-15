d3d91cf3fb7797d8581280b9e14eaa30014c34e3
이재문 <990914s@gmail.com>
2025-12-15 15:36:23 +0900

    WIP

---
+++ Assets/Scripts/Data/CSV/Data/PartySynergyData.cs
@@ -0,0 +1,23 @@
+public class PartySynergyData
+{
+    public int Party_ID { get; set; }
+    public int Party_Name_ID { get; set; }
+    public int Party_Size { get; set; }
+    public int Req_Char_1_ID { get; set; }
+    public int Req_Char_2_ID { get; set; }
+    public int Party_Upgrade_Lv1_ID { get; set; }
+    public int Effect_1_ID { get; set; }
+    public int Effect_1_Value { get; set; }
+    public int Party_Upgrade_Lv2_ID { get; set; }
+    public int Effect_2_ID { get; set; }
+    public int Effect_2_Value { get; set; }
+    public int Party_Upgrade_Lv3_ID { get; set; }
+    public int Effect_3_ID { get; set; }
+    public int Effect_3_Value { get; set; }
+    public int Party_Upgrade_Lv4_ID { get; set; }
+    public int Effect_4_ID { get; set; }
+    public int Effect_4_Value { get; set; }
+    public int Party_Upgrade_Lv5_ID { get; set; }
+    public int Effect_5_ID { get; set; }
+    public int Effect_5_Value { get; set; }
+}
